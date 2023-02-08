d07c3c8a7365e76a44b7e76fc1d5b19e361609a9
acostavs <acostavs1015@gmail.com>
2023-02-08 06:53:49 +0000

    submitting lab 12

--- js/app.js
+++ js/app.js
@@ -2,20 +2,23 @@
 
 let image1 = document.querySelector('section img:nth-child(2)');
 let image2 = document.querySelector('section img:nth-child(3)');
-let image3 = document.querySelector('section img:nth-child(4)')
-let container = document.querySelector(`section`)
-let myButton = document.querySelector(`section + div`)
-container.addEventListener(`click`, myEventHandler)
+let image3 = document.querySelector('section img:nth-child(4)');
+let container = document.querySelector(`section`);
+container.addEventListener(`click`, myEventHandler);
+let myButton = document.querySelector(`section + div`);
+
+let indexArr = [];
+
 
 function Pic(name, fileExt = `jpg`) {
     this.productName = name;
-    this.src = `../img/images/${name}.${fileExt}`;
+    this.src = `./img/images/${name}.${fileExt}`;
     this.views = 0;
-    this.likes = 0
+    this.likes = 0;
 
 };
 
-let wineGlass = new Pic(`wine-glassjpg`);
+let wineGlass = new Pic(`wine-glass`);
 let waterCan = new Pic(`water-can`);
 let unicorn = new Pic(`unicorn`);
 let tauntaun = new Pic(`tauntaun`);
@@ -28,7 +31,7 @@ let dragon = new Pic(`dragon`);
 let dogDuck = new Pic(`dog-duck`);
 let cthulhu = new Pic(`cthulhu`);
 let chair = new Pic(`chair`);
-let bubbleGum = new Pic(`bubblegumjpg`);
+let bubbleGum = new Pic(`bubblegum`);
 let breakfast = new Pic(`breakfast`);
 let boots = new Pic(`boots`);
 let bathroom = new Pic(`bathroom`);
@@ -40,28 +43,46 @@ let pictureArray = [wineGlass, waterCan, unicorn, tauntaun,
     cthulhu, chair, bubbleGum, breakfast, boots, bathroom,
     banana, bag
 ];
+console.log(pictureArray)
 
 let startingClicksCount = 0;
 let clicksAllowed = 25;
-
 
 function getRandomNumber() {
     return Math.floor(Math.random() * pictureArray.length);
 };
 
+
 function renderPics() {
-    let randomPicOne = getRandomNumber()
-    let randomPicTwo = getRandomNumber()
-    let randomPicThree = getRandomNumber()
+    // let randomPicOne = getRandomNumber()
+    // let randomPicTwo = getRandomNumber()
+    // let randomPicThree = getRandomNumber()
+
+    while (indexArr.length < 19) {
+        let num = getRandomNumber();
+        // console.log(num)
+        if (!indexArr.includes(num)) {
+            indexArr.push(num);
+
+        }
+    }
+
+    console.log(indexArr);
+
+    let randomPicOne = indexArr.shift();
+    let randomPicTwo = indexArr.shift();
+    let randomPicThree = indexArr.shift();
+    console.log(randomPicOne)
+
     image1.src = pictureArray[randomPicOne].src;
     image2.src = pictureArray[randomPicTwo].src;
     image3.src = pictureArray[randomPicThree].src;
-    image1.alt = pictureArray[randomPicOne].productName
-    image2.alt = pictureArray[randomPicTwo].productName
-    image3.alt = pictureArray[randomPicThree].productName
-    pictureArray[randomPicOne].views++
-    pictureArray[randomPicTwo].views++
-    pictureArray[randomPicThree].views++
+    image1.alt = pictureArray[randomPicOne].productName;
+    image2.alt = pictureArray[randomPicTwo].productName;
+    image3.alt = pictureArray[randomPicThree].productName;
+    pictureArray[randomPicOne].views++;
+    pictureArray[randomPicTwo].views++;
+    pictureArray[randomPicThree].views++;
     // console.log(pictureArray[picOne])
     // console.log(pictureArray)
 
@@ -74,7 +95,7 @@ function renderPics() {
 
 function myEventHandler(e) {
     startingClicksCount++
-    console.log(startingClicksCount)
+    // console.log(startingClicksCount)
     let chosenPic = e.target.alt
     for (let i = 0; i < pictureArray.length; i++) {
         if (pictureArray[i].productName === chosenPic) {
@@ -82,22 +103,53 @@ function myEventHandler(e) {
         }
     }
     renderPics()
-    console.log(pictureArray)
+    // console.log(pictureArray)
 
     function renderResults() {
-        let results = document.querySelector('ul')
+        let results = document.querySelector('ul');
         for (let j = 0; j < pictureArray.length; j++) {
             let resultLi = document.createElement(`li`)
             resultLi.textContent = `${pictureArray[j].productName} had ${pictureArray[j].views} views and ${pictureArray[j].likes} likes.`
             results.appendChild(resultLi)
-        }
-    }
+        };
+    };
 
     if (startingClicksCount === clicksAllowed) {
         container.removeEventListener(`click`, myEventHandler)
         myButton.addEventListener(`click`, renderResults)
+        renderChart();
+    };
+};
+
+
+function renderChart() {
+    let productLikes = [];
+    let product = [];
+    for (let i = 0; i < pictureArray.length; i++) {
+        productLikes.push(pictureArray[i].likes)
+        product.push(pictureArray[i].productName)
     }
-}
+    console.log(productLikes)
+    const ctx = document.getElementById('myChart');
+    let config = {
+        type: 'bar',
+        data: {
+            labels: product,
+            datasets: [{
+                label: '# of Votes',
+                data: productLikes,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    }
 
-
-renderPics()
+    new Chart(ctx, config);
+};
+renderPics();
